33bd744bbabd447dbada2683f9a59b4a467d0a55
ippon <ippon@ippon-laptop.(none)>
2012-03-07 17:13:51 +0100

    maj

--- src/main/java/fr/quoteBrowser/BashDotOrgQuoteProvider.java
+++ src/main/java/fr/quoteBrowser/BashDotOrgQuoteProvider.java
@@ -6,7 +6,13 @@ import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
+
+import android.graphics.Color;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 
 public class BashDotOrgQuoteProvider implements QuoteProvider {
 
@@ -16,10 +22,26 @@ public class BashDotOrgQuoteProvider implements QuoteProvider {
 		Document doc = Jsoup.connect("http://bash.org/?latest").get();
 		Elements quotesElts = doc.select("p.qt");
 		for (Element quotesElt : quotesElts) {
-			  CharSequence quoteText = quotesElt.text();
-			  quotes.add(new Quote(quoteText));
+			  CharSequence quoteText = Html.fromHtml(new TextNode(quotesElt.html(),"").getWholeText());
+			  quotes.add(new Quote(colorizeUsernames(quoteText)));
 			}
 		return quotes.toArray(new Quote[quotes.size()]);
+	}
+
+	private CharSequence colorizeUsernames(CharSequence quoteText) {
+		SpannableStringBuilder ssb = new SpannableStringBuilder(quoteText);
+		
+		int currentIndex=0;
+		while(currentIndex<quoteText.length()){
+			int indexBaliseOuvrante=quoteText.toString().indexOf("<",currentIndex);
+			int indexBaliseFermante=quoteText.toString().indexOf(">",indexBaliseOuvrante);
+			if (indexBaliseOuvrante>-1 && indexBaliseFermante>-1){
+				ssb.setSpan(new ForegroundColorSpan(Color.BLUE), indexBaliseOuvrante, indexBaliseFermante+1, 0);
+				currentIndex=indexBaliseFermante+1;
+			}else currentIndex=quoteText.length()+1;
+			
+		}
+		return ssb;
 	}
 
 }
