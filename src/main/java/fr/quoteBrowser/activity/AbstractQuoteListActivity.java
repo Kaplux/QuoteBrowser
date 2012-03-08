463be0561173938badfc2e78e704362e258d6c66
ippon <ippon@ippon-laptop.(none)>
2012-03-08 11:31:42 +0100

    added loading screen

--- src/main/java/fr/quoteBrowser/activity/AbstractQuoteListActivity.java
+++ src/main/java/fr/quoteBrowser/activity/AbstractQuoteListActivity.java
@@ -2,55 +2,76 @@ package fr.quoteBrowser.activity;
 
 import java.io.IOException;
 
-import fr.quoteBrowser.Quote;
-import fr.quoteBrowser.R;
-import fr.quoteBrowser.R.layout;
-
+import android.app.Activity;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import fr.quoteBrowser.Quote;
+import fr.quoteBrowser.R;
 
 public abstract class AbstractQuoteListActivity extends ListActivity {
 
-	
-    private static String TAG = "quoteBrowser";
+	private static String TAG = "quoteBrowser";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-
-    	
-		try {
-			Quote[] quotes = getQuotes();
-		
-    	
-    	setListAdapter(new ArrayAdapter<Quote>(this, R.layout.quote_list_item_layout, quotes){
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		final Activity currentActivity=this;
+		final ProgressDialog progressDialog = ProgressDialog.show(
+				this, "Loading", "please wait", true);
+		new AsyncTask<Void, Void, Quote[]>() {
 
 			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
-				TextView v=(TextView) super.getView(position, convertView, parent);
-				v.setText(getItem(position).getQuoteText());
-				return v;
-			}});
-		} catch (IOException e) {
-			Log.e(TAG, e.getMessage());
-		}
-  	
-    }
+			protected Quote[] doInBackground(Void... params) {
+				Quote[] quotes=new Quote[0];
+				try {
+					quotes= getQuotes();
+				} catch (IOException e) {
+					Log.e(TAG, e.getMessage());
+				}
+				return quotes;
+			}
+
+			protected void onPreExecute() {
+				super.onPreExecute();
+			}
+
+			protected void onPostExecute(Quote[] quotes) {
+				progressDialog.dismiss();
+				setListAdapter(new ArrayAdapter<Quote>(currentActivity,
+						R.layout.quote_list_item_layout, quotes) {
+
+					@Override
+					public View getView(int position, View convertView,
+							ViewGroup parent) {
+						TextView v = (TextView) super.getView(position,
+								convertView, parent);
+						v.setText(getItem(position).getQuoteText());
+						return v;
+					}
+				});
+
+			}
+
+		}.execute();
+
+	}
 
 	protected abstract Quote[] getQuotes() throws IOException;
-    
-    
 
 }
-
