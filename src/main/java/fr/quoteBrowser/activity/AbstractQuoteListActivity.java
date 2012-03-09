5e3de097288c6483fee85cfa3141d9e5ec1ea0c9
ippon <ippon@ippon-laptop.(none)>
2012-03-09 16:16:00 +0100

    changed listview style

--- src/main/java/fr/quoteBrowser/activity/AbstractQuoteListActivity.java
+++ src/main/java/fr/quoteBrowser/activity/AbstractQuoteListActivity.java
@@ -45,7 +45,10 @@ public abstract class AbstractQuoteListActivity extends Activity {
 				"Loading", "please wait", true);
 		WebView bannerAdView = (WebView) findViewById(R.id.bannerAdView);
 		bannerAdView.getSettings().setJavaScriptEnabled(true);
-		bannerAdView.loadData("<script type=\"text/javascript\" src=\"http://ad.leadboltads.net/show_app_ad.js?section_id=593079000\"></script>","text/html",null);
+		bannerAdView
+				.loadData(
+						"<script type=\"text/javascript\" src=\"http://ad.leadboltads.net/show_app_ad.js?section_id=593079000\"></script>",
+						"text/html", null);
 		new AsyncTask<Void, Void, List<Quote>>() {
 
 			@Override
@@ -72,19 +75,31 @@ public abstract class AbstractQuoteListActivity extends Activity {
 					@Override
 					public View getView(int position, View convertView,
 							ViewGroup parent) {
-						LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					    View  view = layoutInflater.inflate(R.layout.quote_list_item_layout, null);
-					    Quote quote=getItem(position);
-						((TextView)view.findViewById(R.id.quoteItemTextView)).setText(quote.getQuoteText());
-						((TextView)view.findViewById(R.id.quoteItemSourceView)).setText("source: " + quote.getQuoteSource());
-						((TextView)view.findViewById(R.id.quoteItemTitleView)).setText("id: "+quote.getQuoteTitle());
-						if (quote.getQuoteScore()!=null){
-							((TextView)view.findViewById(R.id.quoteItemScoreView)).setText("score: "+quote.getQuoteScore());
+						LayoutInflater layoutInflater = (LayoutInflater) getContext()
+								.getSystemService(
+										Context.LAYOUT_INFLATER_SERVICE);
+						View view = layoutInflater.inflate(
+								R.layout.quote_list_item_layout, null);
+						Quote quote = getItem(position);
+						((TextView) view.findViewById(R.id.quoteItemTextView))
+								.setText(quote.getQuoteText());
+						((TextView) view.findViewById(R.id.quoteItemSourceView))
+								.setText("source: " + quote.getQuoteSource());
+						((TextView) view.findViewById(R.id.quoteItemTitleView))
+								.setText("id: " + quote.getQuoteTitle());
+						if (quote.getQuoteScore() != null) {
+							((TextView) view
+									.findViewById(R.id.quoteItemScoreView))
+									.setText("score: " + quote.getQuoteScore());
 						}
 						return view;
 					}
+					@Override
+					public boolean isEnabled(int position) {
+						return false;
+					}
 				});
-				
+
 			}
 
 		}.execute();
